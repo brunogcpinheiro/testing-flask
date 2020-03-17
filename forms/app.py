24ec8c4ab0e2117034e83d64d1292984aa239ae2
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2020-03-17 01:01:18 -0300

    redirect to home if GET /user

--- forms/app.py
+++ forms/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,9 +8,12 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/user", methods=["POST"])
+@app.route("/user", methods=["GET", "POST"])
 def created_user():
-    first_name = request.form.get("first_name")
-    last_name = request.form.get("last_name")
-    age = request.form.get("age")
-    return render_template("created_user.html", first_name=first_name, last_name=last_name, age=age)
+    if request.method == 'GET':
+        return redirect(url_for('index'))
+    else:
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        age = request.form.get("age")
+        return render_template("created_user.html", first_name=first_name, last_name=last_name, age=age)
